9c2ece34350672e0982da10c6065f9e28015654a
Wonhee Han <hanwh@hunature.net>
2020-09-07 16:00:51 +0900

    add exception

--- JavaTest/src/HexaStringToByteArray.java
+++ JavaTest/src/HexaStringToByteArray.java
@@ -5,7 +5,7 @@
 /**
  * example : hexa-string to byte array.
  * @author hbesthee@naver.com
- * @since 2020-09-07
+ * @date 2020-09-07
  */
 public class HexaStringToByteArray {
 
@@ -28,8 +28,16 @@ public class HexaStringToByteArray {
 		System.out.printf("converted Data(0 ~ 3) = %02x %02x %02x %02x\n", convertedData[0], convertedData[1], convertedData[2], convertedData[3]);
 
 //		byte[] notEvenLengthData = javax.xml.bind.DatatypeConverter.parseHexBinary("123");
+		/* Exception in thread "main" java.lang.IllegalArgumentException: hexBinary needs to be even-length: 123
+			at javax.xml.bind.DatatypeConverterImpl.parseHexBinary(DatatypeConverterImpl.java:442)
+			at javax.xml.bind.DatatypeConverter.parseHexBinary(DatatypeConverter.java:357)
+		*/
 
-		byte[] illegalCharacterData = javax.xml.bind.DatatypeConverter.parseHexBinary("12 34 56");
+//		byte[] illegalCharacterData = javax.xml.bind.DatatypeConverter.parseHexBinary("12 34 56");
+		/* Exception in thread "main" java.lang.IllegalArgumentException: contains illegal character for hexBinary: 12 34 56
+			at javax.xml.bind.DatatypeConverterImpl.parseHexBinary(DatatypeConverterImpl.java:451)
+			at javax.xml.bind.DatatypeConverter.parseHexBinary(DatatypeConverter.java:357)
+		*/
 	}
 
 }
